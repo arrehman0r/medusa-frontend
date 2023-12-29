bd8ac155c61b64eaa26fbab1e973e7129cc0f7f5
Abdul Rehman <arrehman0r@gmail.com>
2023-12-29 17:02:54 +0500

    orignal theme

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,7 +5,7 @@ import { Github } from "@medusajs/icons"
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+      {/* <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
@@ -29,7 +29,7 @@ const Hero = () => {
             <Github />
           </Button>
         </a>
-      </div>
+      </div> */}
     </div>
   )
 }
